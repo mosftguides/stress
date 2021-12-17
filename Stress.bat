c3cf1c8778cd7aaddd85d5602e07db7c0875c6ca
mosftguides <95550579+mosftguides@users.noreply.github.com>
2021-12-17 18:15:16 +0100

    Update Stress.bat

--- Stress.bat
+++ Stress.bat
@@ -4,5 +4,9 @@ echo :x >> xx.bat
 echo set /a z=%random%%random%%random%+%random%%random%%random%+%random%%random%%random% >> xx.bat
 echo cls >> xx.bat
 echo goto x >> xx.bat
+echo Select Mode: 
+echo Low-End-CPUs (e.g. Intel Core i3-330UM)
+echo Normal CPUs (e.g Intel Core i5-6200U)
+echo High-End-CPUs (e.g AMD Ryzen 5900X or Intel Core i9-11900K)
 start xx.bat
 start xx.bat
